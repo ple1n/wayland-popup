60c19ac2a988d40abc9a962d6bb2ebd6bddf90b0
plein <68096727+planetorDyd@users.noreply.github.com>
2025-09-22 05:05:31 +0800

    some margin

--- examples/simple_layer.rs
+++ examples/simple_layer.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use egui::{Color32, Stroke, Style, Visuals};
+use egui::{style::Spacing, Color32, Margin, Stroke, Style, Visuals};
 use layer_shell_wgpu_egui::{application::Msg, layer_shell::LayerShellOptions};
 use smithay_client_toolkit::shell::wlr_layer::KeyboardInteractivity;
 
@@ -22,7 +22,7 @@ fn main() -> anyhow::Result<()> {
         let mut li = Visuals::dark();
         li.override_text_color = Some(Color32::WHITE.gamma_multiply(0.7));
         ctx.set_visuals(li);
-        egui::CentralPanel::default().frame(egui::Frame::none().fill(Color32::WHITE.gamma_multiply(0.1))).show(ctx, |ui| {
+        egui::CentralPanel::default().frame(egui::Frame::none().fill(Color32::WHITE.gamma_multiply(0.1)).inner_margin(Margin::same(15.))).show(ctx, |ui| {
             ui.heading("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
             ui.horizontal(|ui| {
                 let name_label = ui.label("Your name: ");
