60e43d23717a78421428d29d6984156e9cbf9de6
plein <68096727+planetorDyd@users.noreply.github.com>
2025-09-22 05:12:41 +0800

    gestured

---
+++ src/bin/gestured.rs
@@ -0,0 +1,7 @@
+//! Privileged process handling all kinds of gestures that may faciliate apps such as a popup dictionary.
+//! Means include hooking up /dev/ inputs, and simulating user inputs.
+//! 
+
+fn main() {
+    
+}
