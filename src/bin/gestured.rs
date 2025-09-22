22422557fff96fdbdaa5a446e9cebef92a1dc24d
plein <68096727+planetorDyd@users.noreply.github.com>
2025-09-22 15:41:20 +0800

    evdev

--- src/bin/gestured.rs
+++ src/bin/gestured.rs
@@ -1,7 +1,28 @@
 //! Privileged process handling all kinds of gestures that may faciliate apps such as a popup dictionary.
 //! Means include hooking up /dev/ inputs, and simulating user inputs.
-//! 
+//!
 
-fn main() {
-    
+use anyhow::Ok as aok;
+use anyhow::Result;
+use futures::{stream::FuturesUnordered, StreamExt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut streams = Vec::new();
+    for (path, dev) in evdev::enumerate() {
+        println!("{:?}", path);
+        let ev = dev.into_event_stream()?;
+        streams.push(ev);
+    }
+    let mut sa = futures::stream::select_all(streams);
+    loop {
+        let ev = sa.next().await;
+        if let Some(ev) = ev {
+            let ev = ev?;
+            println!("{:?}", ev);
+        } else {
+            break;
+        }
+    }
+    aok(())
 }
